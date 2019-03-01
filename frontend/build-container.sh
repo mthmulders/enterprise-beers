18e4ed975e2390ef17a4e0e52cda7e57b7406f70
Maarten Mulders <maartenm@infosupport.com>
2019-03-01 09:38:03 +0100

    Build frontend in parallel, not as part of Maven lifecycle

---
+++ frontend/build-container.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env bash
+
+if [[ "$#" -ne 1 ]]; then
+    echo "$0 <num>"
+    echo ""
+    echo "Parameters:"
+    echo "  <num> the version of the container being built"
+    exit
+fi
+
+pushd app/
+
+npm install
+
+npm run build
+
+popd
+
+tmp_dir=$(mktemp -d)
+mkdir -p ${tmp_dir}/app/build
+
+cp -Rv app/build/* ${tmp_dir}/app/build/
+cp -Rv Dockerfile ${tmp_dir}/
+
+pushd ${tmp_dir}
+
+docker build -t enterprisebeers.azurecr.io/frontend:1.0.$1 .
+docker push enterprisebeers.azurecr.io/frontend:1.0.$1
+
+popd
