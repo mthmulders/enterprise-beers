4e9e10039f436d1497a7f4ffe1c2b65e25d900d2
Maarten Mulders <maartenm@infosupport.com>
2019-03-08 16:08:53 +0100

    Properly return the style of beer

--- beer-api/src/main/java/com/infosupport/beers/model/Beer.java
+++ beer-api/src/main/java/com/infosupport/beers/model/Beer.java
@@ -1,10 +1,10 @@
 package com.infosupport.beers.model;
 
+import java.util.UUID;
+
 import lombok.Builder;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
-
-import java.util.UUID;
 
 @Builder
 @Getter
--- beer-api/src/main/java/com/infosupport/beers/model/Style.java
+++ beer-api/src/main/java/com/infosupport/beers/model/Style.java
@@ -1,10 +1,23 @@
 package com.infosupport.beers.model;
 
-public enum Style {
-    BELGIAN_BLONDE,
-    BELGIAN_STRONG_DARK_ALE,
-    BELGIAN_STRONG_GOLDEN_ALE,
-    BELGIAN_TRIPEL,
-    IPA,
-    WINTER_ALE,
+import javax.json.bind.annotation.JsonbTypeSerializer;
+
+import com.infosupport.beers.common.DisplayableEnum;
+import com.infosupport.beers.common.DisplayableEnumSerializer;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@Getter
+@JsonbTypeSerializer(DisplayableEnumSerializer.class)
+public enum Style implements DisplayableEnum {
+    BELGIAN_BLONDE("Belgian Blonde"),
+    BELGIAN_STRONG_DARK_ALE("Belgian Strong Dark Ale"),
+    BELGIAN_STRONG_GOLDEN_ALE("Belgian Strong Golden Ale"),
+    BELGIAN_TRIPEL("Belgian Tripel"),
+    IPA("IPA"),
+    WINTER_ALE("Winter Ale");
+
+    private final String displayName;
 }
