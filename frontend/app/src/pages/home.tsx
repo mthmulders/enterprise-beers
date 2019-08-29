6007583e5af6fb2576888091ac9aeda70192f8bc
Maarten Mulders <m.th.mulders@xs4all.nl>
2019-08-29 08:12:01 +0200

    Supply an alt tag for the decorative image

--- frontend/app/src/pages/home.tsx
+++ frontend/app/src/pages/home.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Home = () => (
     <>
         <br />
-        <img src="img/Two-pints-beer-main.jpg"></img>
+        <img src='img/Two-pints-beer-main.jpg' alt=''></img>
     </>
 )
 
